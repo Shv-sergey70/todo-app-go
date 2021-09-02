2b5ee6c8716df8fb90246494d86121677c8c6de3
Sergey Shershnev <s.shershnov@lucky-team.pro>
2021-09-02 16:14:48 +0300

    TODO: added migrations & config file

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,28 @@ import (
 	"github.com/Shv-sergey70/todo-app-go/pkg/handler"
 	"github.com/Shv-sergey70/todo-app-go/pkg/repository"
 	"github.com/Shv-sergey70/todo-app-go/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	server := new(todo.Server)
 
-	if err := server.Run("8083", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }

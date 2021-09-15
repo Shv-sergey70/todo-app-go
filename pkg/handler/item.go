031537473af0b3f8a4ef517793ef0c1bb615b2bd
Sergey Shershnev <s.shershnov@lucky-team.pro>
2021-09-15 15:45:11 +0300

    Todo: part 11

--- pkg/handler/item.go
+++ pkg/handler/item.go
@@ -1,13 +1,60 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Shv-sergey70/todo-app-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
 func (h *Handler) createItem(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
 
+	listIdInt, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id parameter")
+		return
+	}
+
+	var input todo.TodoItem
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.service.TodoItem.Create(userId, listIdInt, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) getAllItems(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
 
+	listIdInt, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id parameter")
+		return
+	}
+
+	items, err := h.service.TodoItem.GetAll(userId, listIdInt)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, items)
 }
 
 func (h *Handler) getItemById(c *gin.Context) {
